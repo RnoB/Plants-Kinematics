15fc70d87793c1acfcc8e081722b2d2c456e4886
rnoB <rno.bstin@gmail.com>
2018-06-01 19:44:22 +0200

    First working version with growth

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Plants-Kinematics
 Python software to model the growth of plants
+
+
+import plantsKin as pk
+from math import pi
+
+A tilted virtual plant is created with the following command
+
+plant = pk.Plant(x0 = [0,0,0],theta0 = pi/2,N = 100,dt=.1,growth = 'no')
+plant = pk.Plant(x0 = [0,0,0],theta0 = pi/2,N = 100,dt=.1,growth = 'Apical',growthZone = 1,growthRate = 1)
+
+then a few interaction should be added
+
+plant.addInteractions(name = 'Proprioception' ,intensity=1)
+plant.addInteractions(name = 'Tropism' ,intensity=1,direction = 0)
--- plantsKin.py
+++ plantsKin.py
@@ -1,6 +1,6 @@
 import numpy as np
 import math
-
+import time
 
 InteractionsName = ['Tropism','ApicalTropism','Proprioception']
 GrowthMode = ['Apical','Exponential']
@@ -9,21 +9,9 @@ GrowthMode = ['Apical','Exponential']
 
 class skeletonElements:
 
-    x = np.zeros((1,3))
-    theta = 0
-    curvature = 0
-    s = 0
-    ds = .1
-    psiC = 0
-    psiG = 0
 
-    theta0 = 0
-    thetaApical =0
-    psi0 = 0
-    interactions = []
-    growth = {'name':'no'}
 
-    def __init__(self,x,theta0,curvature0,s,ds,psi0 = 0,psiC0 = 0,psiG0 = 0,dt = .1):
+    def __init__(self,x,theta0,curvature0,s,ds,psi0 = 0,psiC0 = 0,psiG0 = 0,dt = .1,growth = {'name':'no','intensity':1,'direction':0}):
         
         self.x = x
         self.theta = theta0
@@ -34,6 +22,8 @@ class skeletonElements:
         self.psiG = psiG0
         self.psi0 = psi0
         self.dt = dt
+        self.growth = growth.copy()
+        self.interactions = []
 
     def updateCurvilinearAbscissa(self,s):
         self.s = s
@@ -45,33 +35,45 @@ class skeletonElements:
         self.theta = theta  
 
     def updateSpatialPosition(self,x0,theta0,psi0,ds0):
-        self.x[0]  = x0[0] + self.ds0 * (np.cos(theta0) * np.cos(psi0))
-        self.x[1]  = x0[1] + self.ds0 * (np.cos(theta0) * np.sin(psi0))
-        self.x[2]  = x0[2] + self.ds0 * (np.sin(theta0))
+        self.x[0]  = x0[0] + ds0 * (np.sin(theta0) * np.cos(psi0))
+        self.x[1]  = x0[1] + ds0 * (np.sin(theta0) * np.sin(psi0))
+        self.x[2]  = x0[2] + ds0 * (np.cos(theta0))
 
 
     def addInteractions(self,name,intensity=0,direction = 0):
+        
         self.interactions.append({'name':name,'intensity':intensity,'direction':direction})
+        
+
 
     def update(self):
+        t0 = time.time()
+
         delta = 0
         deltaParrallel = 0
         deltaPerpendicular = 0
+
         for interaction in self.interactions:
-            if interaction.name == 'Proprioception':
-                deltaParrallel += self.growthRate * interaction.intensity * self.cuvature * self.dt
+            
+            interactionName = interaction['name']
+            if interaction['name'] == 'Proprioception':
+                deltaParrallel +=interaction['intensity'] * self.curvature 
             else:
-                if interaction.name == 'Tropism':
-                    delta += self.growthRate * interaction.intensity * (self.theta - interaction.direction) * self.dt
-                if interaction.name == 'ApicalTropism':
-                    delta += self.growthRate * interaction.intensity * (self.thetaApical - interaction.direction) * self.dt
-                deltaParrallel += delta * cos(self.psiG - self.psiC)
-                delataPerpendicular += delta * sin(self.psiG - self.psiC)
-        self.curvature = deltaParrallel * self.growthRate * self.dt
-        if math.abs(self.curvature) >0:
-            self.psiC = (deltaParrallel * self.growthRate * self.dt)/self.curvature
-        if self.growth.name in GrowthMode:
-            self.ds += self.ds * self.growth.rate * self.dt 
+                if interaction['name'] == 'Tropism':
+                    delta += interaction['intensity'] * (self.theta - interaction['direction'])
+                elif interaction['name'] == 'ApicalTropism':
+                    delta += interaction['intensity'] * (self.thetaApical - interaction['direction'])
+        deltaParrallel +=   delta * np.cos(self.psiG - self.psiC)
+        deltaPerpendicular +=   delta * np.sin(self.psiG - self.psiC)
+        
+        
+        self.curvature += deltaParrallel * self.growth['growthRate'] * self.dt
+        if np.abs(self.curvature) >0:
+            self.psiC = (deltaPerpendicular * self.growth['growthRate'] * self.dt)/self.curvature
+        if self.growth['name'] in GrowthMode:
+
+            self.ds += self.ds * self.growth['growthRate'] * self.dt 
+
 
 
 
@@ -81,20 +83,7 @@ class skeletonElements:
 class Plant:
 
 
-    x0 = np.zeros((1,3))
 
-    theta0 = 0
-    curvature0 = 0
-    length0 = 1
-    length = 1
-
-    psi0 = 0
-    ds = .1
-    dt = .1
-    skeleton = []
-
-    interactions = []
-    growth = {'no'}
     def __init__(self,x0,theta0=0,curvature0=0,length0 = 1,psi0 = 0,psiC0 = 0,psiG0 = 0,N = 100,dt=.1,growth = 'no',growthZone = 1,growthRate = 1):
         
         self.x = []
@@ -111,6 +100,9 @@ class Plant:
         self.ds = length0/N
         self.psi0 = psi0
         self.dt = dt
+        self.skeleton = []
+
+        self.interactions = []
 
         x = self.x0
         theta = self.theta0
@@ -120,33 +112,37 @@ class Plant:
 
         s = 0
 
-        self.skeleton.append(skeletonElements(np.copy(x),theta,curvature,0,self.ds,self.psi0,psiC0,psiG0,self.dt))
+        self.skeleton.append(skeletonElements(np.copy(x),theta,curvature,0,self.ds,self.psi0,psiC0,psiG0,self.dt,growth=self.growth))
         
 
         for k in range(1,N):
             
-            x[0]  = x[0] + self.ds * (np.cos(theta0) * np.cos(psi0))
-            x[1]  = x[1] + self.ds * (np.cos(theta0) * np.sin(psi0))
-            x[2]  = x[2] + self.ds * (np.sin(theta0))
+            x[0]  = x[0] + self.ds * (np.sin(theta0) * np.cos(psi0))
+            x[1]  = x[1] + self.ds * (np.sin(theta0) * np.sin(psi0))
+            x[2]  = x[2] + self.ds * (np.cos(theta0))
             s += self.ds
-            self.skeleton.append(skeletonElements(np.copy(x),theta,curvature,s,self.ds,self.psi0,psiC0,psiG0,self.dt))
+            self.skeleton.append(skeletonElements(np.copy(x),theta,curvature,s,self.ds,self.psi0,psiC0,psiG0,self.dt,growth=self.growth))
         self.flatten()
 
     def updateSpatialPosition(self):
         s = 0
         for k in range(1,len(self.skeleton)):
-            s += self.ds
-            self.skel[k].updateCurvilinearAbscissa(s)
-            self.skel[k].updateOrientation(self.skel[k-1].theta + self.skel[k-1].curvature * self.skel[k-1].ds)
-            self.skel[k].updateSpatialPosition(self.skel[k-1].x,self.skel[k-1].theta,self.skel[k-1].psiC,self.skel[k-1].ds)
+            s += self.skeleton[k-1].ds
+            self.skeleton[k].updateCurvilinearAbscissa(s)
+            self.skeleton[k].updateOrientation(self.skeleton[k-1].theta + self.skeleton[k-1].curvature * self.skeleton[k-1].ds)
+            self.skeleton[k].updateSpatialPosition(self.skeleton[k-1].x,self.skeleton[k-1].theta,self.skeleton[k-1].psiC,self.skeleton[k-1].ds)
             
 
 
     def addInteractions(self,name,intensity=0,direction = 0):
         if name in InteractionsName:
             self.interactions.append({'name':name,'intensity':intensity,'direction':direction})
+            
             for skel in self.skeleton:
                 skel.addInteractions(name,intensity,direction)
+                
+            
+            
         else:
             print(' --- '+name+' is not part of the known interactions ')
             print(' --- please use one of the following interaction :')
@@ -162,18 +158,29 @@ class Plant:
         self.length = self.s[-1]
 
 
+
     def updateGrowth(self):
-        if self.growth.name in GrowthMode:
+        
+        if self.growth['name'] in GrowthMode:
             for skel in self.skeleton:
-                if growth.name == 'Exponential':
-                    skel.growthRate = self.growth.growthRate 
-                if growth.name == 'Apical':
-                    if (self.length-skel.s) < growth.growthZone:
-                        skel.growthRate = self.growth.growthRate
+                
+                if self.growth['name'] == 'Exponential':
+                    skel.growth['growthRate'] = self.growth['growthRate'] 
+                if self.growth['name'] == 'Apical':
+                    
+                    if (self.length-skel.s) < self.growth['growthZone']:
+
+                        skel.growth['growthRate'] = self.growth['growthRate']
+                    else:
+                        
+                        skel.growth['growthRate'] = 0
 
     def update(self):
+        self.updateGrowth()
         for skel in self.skeleton:
             skel.update()
+        self.updateSpatialPosition()
+        self.flatten()
 
 
         
